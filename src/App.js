5a564121b5ea6bab833d80df4d04d1941665fd7c
Ryan Burns <ryankburns@gmail.com>
2021-09-05 20:30:49 -0700

    added Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import classes from './App.module.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import classes from './App.module.css';
 import Header from './components/Header'
 
 
 function App() {
   return(
-    // <Router>
-      <Header />
-    // </Router>
+    <Router>
+      <Route path="/" component={Header} />
+    </Router>
   )
 }
 
