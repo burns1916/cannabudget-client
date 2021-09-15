c4f738e9643c68318d309ccad95bfb09ed0eb2c7
Ryan Burns <ryankburns@gmail.com>
2021-09-14 18:06:24 -0700

    added logout component

---
+++ src/components/logout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../actions/currentUser';
+import { withRouter } from 'react-router-dom';
+
+const Logout = ({ logout, history }) => {
+    return (
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            logout(history)
+        }
+    } >
+    <input type="submit" value="Log Out" />
+    </form>
+    )
+}
+
+export default withRouter(connect(null, { logout })(Logout))
