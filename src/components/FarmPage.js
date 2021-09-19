1bec5f31d75552bb2848b28e833cdd0ba4267fd2
Ryan Burns <ryankburns@gmail.com>
2021-09-18 21:53:47 -0700

    added Farm Page

---
+++ src/components/FarmPage.js
@@ -0,0 +1,33 @@
+import React { Component } from "react";
+import { connect } from "react-redux";
+import { setFarm, unsetFarm} from '../actions/farms'
+
+class FarmPage extends Component {
+
+    componentDidMount() {
+        const id = this.props.match.params.id
+        this.props.setFarm(id)
+    }
+
+    componentWillUnmount() {
+        this.props.unsetFarm()
+    }
+
+    render() {
+        const { name, location, yield} = this.props
+        return (
+            <>
+                <h2>Farm Name: { name }</h2>
+                <h3>Location: { location } </h3>
+                <h4>Current Total Yield: { yield }</h4>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    ...state.farms.selectedFarm,
+    currentUser: state.currentUser.currentUser
+})
+
+export default connect(mapStateToProps, { setFarm, unsetFarm })(FarmPage)
