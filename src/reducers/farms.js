8ff981a067d8420ec9b27c6a5651d356d48a3bf6
Ryan Burns <ryankburns@gmail.com>
2021-09-21 20:33:43 -0700

    added crop actions to reducer

--- src/reducers/farms.js
+++ src/reducers/farms.js
@@ -5,10 +5,19 @@ import {
     EDIT_FARM,
     SET_FARM,
     UNSET_FARM,
+    ADD_CROP,
+    DELETE_CROP,
+    GET_CROPS,
+    EDIT_CROP,
+    SET_CROP,
+    UNSET_CROP,
 } from '../actionTypes'
 
 const initialState = {
-    farms: {},
+    farms: [],
+    selectedFarm: null,
+    crops: [],
+    selectedCrop: null,
 }
 
 export function farmsReducer(state = initialState, action) {
@@ -16,7 +25,7 @@ export function farmsReducer(state = initialState, action) {
         case GET_FARMS:
             return {...state, farms: action.farms}
         case ADD_FARM:
-            return {...state, farms: [...state.farms, action.farms]}
+            return {...state, farms: [...state.farms, action.farm]}
         case EDIT_FARM:
             return {...state, farms: [...state.farms.map(farm => farm.id === action.id ? action.farm : farm)]}
         case DELETE_FARM:
@@ -25,6 +34,18 @@ export function farmsReducer(state = initialState, action) {
             return {...state, selectedFarm: action.farm}
         case UNSET_FARM:
             return {...state, selectedFarm: null}
+        case GET_CROPS:
+            return {...state, crops: action.crops, selectedFarm: action.crops}
+        case ADD_CROP:
+            return {...state, crops: [...state.crops, action.crop], selectedFarm: action.crop}
+        case EDIT_CROP:
+            return {...state, crops: [...state.crops.map(crop => crop.id === action.id ? action.crop : crop)], selectedFarm: action.crop}
+        case SET_CROP:
+            return {...state, selectedCrop: action.crop, selectedFarm: action.crop}
+        case UNSET_CROP:
+            return {...state, selectedCrop: null}
+        case DELETE_CROP:
+            return {...state, selectedfarm: action.crop}
         default:
             return state
     }
