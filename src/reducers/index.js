2a8ee429048b6bfcc46ff3541d5fc447f04e7cf2
Ryan Burns <ryankburns@gmail.com>
2021-09-21 21:30:11 -0700

    removed tranaction components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
 // import { currentUserReducer } from './currentUser';
-import { incomeTransactionReducer } from './incomeTransactions';
-import { expenseTransactionReducer } from './expenseTransactions';
 import { currentUserReducer } from './currentUser'
 import { farmsReducer } from './farms'
 
@@ -8,8 +6,6 @@ import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     currentUser: currentUserReducer,
-    incomeTransactions: incomeTransactionReducer,
-    expenseTransactions: expenseTransactionReducer,
     farms: farmsReducer,
 
 })
