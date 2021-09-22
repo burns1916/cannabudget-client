4d2342ed52b11c4046ea43b0638066f739e72d19
Ryan Burns <ryankburns@gmail.com>
2021-09-21 19:45:29 -0700

    added crop actions

--- src/actions/farms.js
+++ src/actions/farms.js
@@ -5,12 +5,19 @@ import {
     EDIT_FARM,
     SET_FARM,
     UNSET_FARM,
+    ADD_CROP,
+    DELETE_CROP,
+    GET_CROPS,
+    EDIT_CROP,
+    SET_CROP,
+    UNSET_CROP,
 
 } from '../actionTypes'
 import { getCurrentUser } from './currentUser'
 
 const BASE_URL = 'http://localhost:3001'
 const FARM_URL = `${BASE_URL}/farms`
+const CROP_URL = `${BASE_URL}/crops`
 
 export const getFarms = () => {
     return (dispatch) => {
@@ -45,7 +52,6 @@ export const addFarm = (farmData) => {
         const sendableFarmData = {
             name: farmData.name,
             location: farmData.location,
-            total: farmData.total,
         }
         return fetch(FARM_URL, {
             credentials: "include",
@@ -95,11 +101,10 @@ export const editFarm = (farmData) => {
         const sendableFarmData = {
             name: farmData.name,
             location: farmData.location,
-            total: farmData.total,
         }
         return fetch(`${FARM_URL}/${farmData.id}`, {
             credentials: "include",
-            method: "POST",
+            method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
             },
@@ -120,5 +125,114 @@ export const editFarm = (farmData) => {
         .catch((error) => {
             console.log('Error:', error)
         });
+    }
+}
+
+export const getCrops = () => {
+    return (dispatch) => {
+        fetch(CROP_URL, {
+            credentials: "include",
+        })
+        .then(resp => resp.json())
+        .then(crops => dispatch({
+            type: GET_CROPS,
+            crops
+        }))
+    }
+}
+
+export const setCrop = (cropId) => {
+    return dispatch => {
+        fetch(`${BASE_URL}/${cropId}`, {
+            credentials: "include",
+        })
+        .then(resp => resp.json())
+        .then(crop => dispatch({
+            type: SET_CROP,
+            crop
+        }))
+    }
+}
+
+export const unsetCrop = () => ({ type: UNSET_CROP })
+
+export const addCrop = (cropData) => {
+    return dispatch => {
+        const sendableCropData = {
+            strain_name: cropData.strain_name,
+            harvest_date: cropData.harvest_date,
+        }
+    return fetch(CROP_URL, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sendableCropData),
+    })
+    .then(response => response.json())
+    .then(crop => {
+        if (crop.error) {
+            alert(crop.error)
+        } else {
+            dispatch({
+                type: ADD_CROP,
+                crop
+            })
+            dispatch(getCurrentUser())
+        }
+    })
+    .catch((error) => {
+        console.log('Error:', error)
+    });
+    }
+}
+
+export const deleteCrop = (cropId) => {
+    return (dispatch) => {
+        return fetch(`${CROP_URL}/${cropId}`, {
+            credentials: "include",
+            method: "DELETE",
+        })
+        .then(resp => resp.json())
+        .then(() => {
+            dispatch({
+                type: DELETE_CROP,
+                cropId
+            })
+            dispatch(getCurrentUser())
+        })
+    }
+}
+
+export const editCrop = (cropData) => {
+    return dispatch => {
+        const sendableCropData = {
+            strain_name: cropData.strain_name,
+            harvest_date: cropData.harvest_date,
+        }
+    return fetch(`${CROP_URL}/${cropData.id}`, {
+        credentials: "include",
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sendableCropData),
+    })
+    .then(response => response.json())
+    .then(crop => {
+        if (crop.error) {
+            alert(crop.error)
+        } else {
+            dispatch({
+                type: EDIT_CROP,
+                crop
+            })
+            dispatch(getCurrentUser())
+        }
+    })
+    .catch((error) => {
+        console.log('Error:', error)
+    });
     }
 }
