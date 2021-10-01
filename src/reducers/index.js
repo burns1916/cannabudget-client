c458d642a665521c75b0255262fa574d3934d063
Ryan Burns <ryankburns@gmail.com>
2021-09-30 19:52:56 -0700

    added crops reducer to index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 // import { currentUserReducer } from './currentUser';
 import { currentUserReducer } from './currentUser'
 import { farmsReducer } from './farms'
-
 import { combineReducers } from 'redux';
+import { cropsReducer } from './crops';
 
 const rootReducer = combineReducers({
     currentUser: currentUserReducer,
     farms: farmsReducer,
+    crops: cropsReducer,
 
 })
 
