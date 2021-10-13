bba9181d93bbbeb81071c9dcaaf2e9f6f079cd27
Ryan Burns <ryankburns@gmail.com>
2021-10-13 16:45:51 -0700

    finished income and expense forms

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ExpenseForm = props => {
+    const [ name, amount, onChange, onSubmit ] = props
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <label>Name:</label>
+                <input placeholder="Name" value={name} type="text" name="name" onChange={onChange} />
+                <label>Amount:</label>
+                <input placeholder="Amount" value={amount} type="text" name="amount" onChange={onChange} />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default ExpenseForm
