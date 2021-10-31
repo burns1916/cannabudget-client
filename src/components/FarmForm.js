a86285f717c11273d14c6c29915c52761f9487af
Ryan Burns <ryankburns@gmail.com>
2021-10-30 21:22:37 -0700

    finished location selector

--- src/components/FarmForm.js
+++ src/components/FarmForm.js
@@ -33,9 +33,9 @@ const FarmForm = props => {
                 <option value="Select a State"> -- Select a State -- </option>
                 {states.map((state, pos) => <option key={pos} value={state.isoCode}>{state.name}</option>)}
             </select>
-            <select onChange={handleCityChange}>
+            <select onChange={onChange} value={location} name="location">
                 <option value="Select a City"> -- Select a City -- </option>
-                {cities.map((city, pos) => <option key={pos} value={city.name} >{city.name}</option>)}
+                {cities.map((city, pos) => <option key={pos} value={city.name}>{city.name}</option>)}
             </select>
             {/* <input placeholder="location" value={location} type="hidden" name="location" className={classes.inputForm}/> */}
             <input type="submit" value="Submit" className={classes.submitBtn} />
